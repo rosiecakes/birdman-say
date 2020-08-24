14aba7e634ed354e06253aa7f89b51e37c812287
Rosie Shimada <shimadar@childandfamilyagency.org>
2020-08-24 17:00:19 -0400

    Updates

---
+++ precache-manifest.fa7101833f00115826f9b732c3506621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de63266f3022e52fe9d79bc2018e1af",
+    "url": "/birdman-say/index.html"
+  },
+  {
+    "revision": "81c41d5fab1166a225cb",
+    "url": "/birdman-say/static/css/main.969e1118.chunk.css"
+  },
+  {
+    "revision": "5631472e49171eb23c70",
+    "url": "/birdman-say/static/js/2.43884e1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/birdman-say/static/js/2.43884e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c41d5fab1166a225cb",
+    "url": "/birdman-say/static/js/main.a2f4949e.chunk.js"
+  },
+  {
+    "revision": "35dab696730057e3fef1",
+    "url": "/birdman-say/static/js/runtime-main.2ffaedfa.js"
+  },
+  {
+    "revision": "dae54c6936667ba35692d6d493a626b5",
+    "url": "/birdman-say/static/media/Harvey280.dae54c69.jpg"
+  }
+]);
