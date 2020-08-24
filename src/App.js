e9e1d627c00e59c87402cec6754d229698f5cfde
Rosie Shimada <shimadar@childandfamilyagency.org>
2020-08-24 17:10:44 -0400

    add title and margin

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App container has-text-centered">
       <header className="App-header">
-        <Quote />        
-        <Birdman />        
+        <Quote />      
+        <Birdman />
       </header>
     </div>
   )
