80912e8227095d7d78d67da9386e67a4a4470e60
Rosie Shimada <shimadar@childandfamilyagency.org>
2020-08-24 17:22:32 -0400

    changed image to background instead of img tag

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
     <div className="App container has-text-centered">
       <header className="App-header">
-        <Quote />      
-        <Birdman />
+        <Quote />
       </header>
     </div>
   )
